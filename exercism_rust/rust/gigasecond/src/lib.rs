d48fb2725fba3d76500f2e9e3800ffb1db874528
susanoo <jeshkumar555@gmail.com>
2017-05-10 06:55:55 +0200

    Exercism: gigasecond submitted

--- exercism_rust/rust/gigasecond/src/lib.rs
+++ exercism_rust/rust/gigasecond/src/lib.rs
@@ -1,3 +1,9 @@
-fn after(start_date: String){
+extern crate chrono;
+use chrono::*;
+
+pub fn after(date_time: DateTime<UTC>) -> DateTime<UTC> {
+
+let ten :i64 = 10;
+date_time + Duration::seconds(ten.pow(9))
 
 }
