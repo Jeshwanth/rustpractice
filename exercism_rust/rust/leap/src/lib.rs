79de61cfe794f1d85492c97d80f65481fc489e32
susanoo <jeshkumar555@gmail.com>
2017-05-25 19:12:02 +0200

    Exercism: Leap

--- exercism_rust/rust/leap/src/lib.rs
+++ exercism_rust/rust/leap/src/lib.rs
@@ -2,11 +2,23 @@
 
 pub fn is_leap_year(year: i32) -> bool
 {
-    if year%4 == 0 { 
-        true 
-    } 
-    else { 
-        false 
-    }
+    if year%4 == 0 {
+        if year%100 == 0 && year%400 == 0
+        {
+            true
+        }
+        else if year%100 == 0
+            {
+                false
+            }
+            else
+            {
+                true
+            }
+        }
+        else
+        {
+            false
+        }
 
 }
