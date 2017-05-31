7ab6765b74ab68b3a6a84dd2adb24608fdf7bf8d
susanoo <jeshkumar555@gmail.com>
2017-05-31 19:32:27 +0200

    Practice: finding factors

---
+++ basicalgos/factorsofx/src/main.rs
@@ -0,0 +1,31 @@
+
+
+fn main() {
+    println!("Finding factors of x!");
+
+	let x = 120;
+	let mut mul = 1;
+    let mut done = false;
+    let mut j = 0;
+    while !done{
+        if (x%mul == 0) && !(j == mul)
+        {
+            j = x/mul;
+            println!("{}",mul );
+            println!("{}",j );
+            mul = mul + 1;
+
+        }
+        else if j == mul
+        {
+            done = true;
+        }
+        else{
+            mul = mul + 1;
+        }
+
+    }
+
+
+
+}
---
+++ exercism_rust/rust/raindrops/src/lib.rs
@@ -0,0 +1,5 @@
+
+pub fn raindrops(num: i32) -> String
+{
+
+}
