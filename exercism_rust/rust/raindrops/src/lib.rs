daebd2fd906e941837538216b2c57ed3d98b6924
susanoo <jeshkumar555@gmail.com>
2017-06-10 07:11:29 +0200

    raindrops

--- exercism_rust/rust/raindrops/src/lib.rs
+++ exercism_rust/rust/raindrops/src/lib.rs
@@ -1,5 +1,27 @@
+use std::str;
 
-pub fn raindrops(num: i32) -> String
-{
+pub fn raindrops() {
+    println!("Finding factors of x!");
 
+	let x = 120;
+	let mut mul = 1;
+    let mut done = false;
+    let mut j = 0;
+    let pling = "Pling";
+    while !done{
+        if (x%mul == 0) && !(j == mul)
+        {
+            j = x/mul;
+            println!("{}",mul );
+            println!("{}",j );
+            mul = mul + 1;
+        }
+        else if j == mul
+        {
+            done = true;
+        }
+        else{
+            mul = mul + 1;
+        }
+    }
 }
