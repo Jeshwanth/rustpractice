642730f86f0f5a8bc18763e287679e0471ba3ffe
susanoo <jeshkumar555@gmail.com>
2017-07-05 19:45:32 +0200

    exercism: beer-song

--- exercism_rust/rust/beer-song/src/lib.rs
+++ exercism_rust/rust/beer-song/src/lib.rs
@@ -12,16 +12,47 @@ pub fn verse(num: u32) -> String {
     else
     {
 
-    let mut owned_string: String = num.to_string().to_owned();
-    let another_owned_string: String = " bottles of beer on the wall,".to_owned();
-    let onemore: String = "bottles of beer.\nTake one down and pass it around,".to_owned();
+    let mut owned_string: String = "".to_owned();
+    let first_string: String = num.to_string().to_owned();
+    let second_string: String = " bottles of beer on the wall, ".to_owned();
+    let third_string: String = " bottles of beer.\nTake one down and pass it around, ".to_owned();
+    let fourth_string: String = (num-1).to_string().to_owned();
+    let mut fifth_string: String = "".to_owned();
+    if (num - 1) == 1 {
+        fifth_string = " bottle of beer on the wall.\n".to_string();
+    }
+    else
+    {
+        fifth_string = " bottles of beer on the wall.\n".to_string();
+    }
 
-    owned_string.push_str(&another_owned_string);
-    owned_string.push_str(&onemore);
+    owned_string.push_str(&first_string);
+    owned_string.push_str(&second_string);
+    owned_string.push_str(&first_string);
+    owned_string.push_str(&third_string);
+    owned_string.push_str(&fourth_string);
+    owned_string.push_str(&fifth_string);
+
     return owned_string;
     }
 }
 
 pub fn sing(num1: u32, num2: u32) -> String {
-    return "Hello".to_string();
+    let mut tempnum1 = num1;
+    let mut owned_string: String = "".to_owned();
+    let mut done = false;
+    while !done{
+        owned_string = owned_string + &verse(tempnum1);
+
+        if tempnum1 == num2
+        {
+            done = true;
+        }
+        else
+        {
+            owned_string.push_str("\n");
+            tempnum1 = tempnum1 - 1;
+        }
+    }
+    return owned_string;
 }
