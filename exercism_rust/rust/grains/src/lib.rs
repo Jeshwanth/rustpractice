a21bb8cdfc9a6b3139eee4c542a7175e4d547fa7
susanoo <jeshkumar555@gmail.com>
2017-07-07 19:45:56 +0200

    exercism: grains 2nd iter

--- exercism_rust/rust/grains/src/lib.rs
+++ exercism_rust/rust/grains/src/lib.rs
@@ -1,19 +1,10 @@
+#Based on http://exercism.io/submissions/68fc84235ed4429a83d66d11f70a51e1
+
 pub fn square(s: u32) -> u64 {
-    if s > 0 && s < 65
-    {
-        return 2u64.pow(s-1)
-    }
-    else
-    {
-        panic!("Square must be between 1 and 64");
-    }
+    if !(s > 0 && s < 65) {panic!("Square must be between 1 and 64");}
+    1 << (s - 1)
 }
 
 pub fn total() -> u64 {
-    let mut sum: u64 = 0;
-    for x in 0..64
-    {
-        sum = sum + 2u64.pow(x);
-    }
-return sum;
+    (1..65).map(square).sum()
 }
